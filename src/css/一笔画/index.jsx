0d7228ba5b938d85f7b8377d8178b38f75aea8fe
qiaodaima <674059309@qq.com>
2020-11-29 03:12:11 +0800

    feat: 添加css-一笔画

---
+++ src/css/一笔画/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import cx from 'classnames';
+import S from './style.less';
+
+export default class extends React.Component {
+    render() {
+        return <div className={S.face} />;
+    }
+}
---
+++ src/css/一笔画/index.md
@@ -0,0 +1,15 @@
+---
+nav:
+    path: '/css'
+    title: 'css'
+    order: 5
+group:
+    path: '/'
+    title: 'css'
+title: '一笔画'
+order: 0
+---
+
+## 用一个标签画画
+
+<code src="./index.jsx" />
